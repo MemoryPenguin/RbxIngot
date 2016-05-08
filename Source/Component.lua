825f689dc6c39e2ed3d2833df5081bd218e06ea8
MemoryPenguin <thecrazyp3nguin+git@gmail.com>
2016-05-07 21:45:15 -0400

    Nuked code duplication
    Auto-generate properties from a list, instead of typing them all out...

--- Source/Component.lua
+++ Source/Component.lua
@@ -64,164 +64,28 @@ end
 --------------------------------------
 -- Since the component doesn't have the default GuiObject primitives they need to be implemented here
 -- This way all components will have them.
-function Component:get_Active()
-	return self.RootObject.Active
-end
+local properties = {
+	"Active", "AbsolutePosition", "AbsoluteSize", "BackgroundColor3", "BackgroundTransparency",
+	"BorderColor3", "BorderSizePixel", "ClipsDescendants", "Draggable", "NextSelectionDown",
+	"NextSelectionLeft", "NextSelectionRight", "NextSelectionUp", "Position", "Rotation",
+	"Selectable", "SelectionImageObject", "Size", "Visible", "ZIndex"
+}
 
-function Component:set_Active(value)
-	self.RootObject.Active = value
-end
+local readOnlyProperties = {
+	AbsolutePosition = true,
+	AbsoluteSize = true
+}
 
-function Component:get_AbsolutePosition()
-	return self.RootObject.AbsolutePosition
-end
-
-function Component:get_AbsoluteSize()
-	return self.RootObject.AbsoluteSize
-end
-
-function Component:get_BackgroundColor3()
-	return self.RootObject.BackgroundColor3
-end
-
-function Component:set_BackgroundColor3(value)
-	self.RootObject.BackgroundColor3 = value
-end
-
-function Component:get_BackgroundTransparency()
-	return self.RootObject.BackgroundTransparency
-end
-
-function Component:set_BackgroundTransparency(value)
-	self.RootObject.BackgroundTransparency = value
-end
-
-function Component:get_BorderColor3()
-	return self.RootObject.BorderColor3
-end
-
-function Component:set_BorderColor3(value)
-	self.RootObject.BorderColor3 = value
-end
-
-function Component:get_BorderSizePixel()
-	return self.RootObject.BorderSizePixel
-end
-
-function Component:set_BorderSizePixel(value)
-	self.RootObject.BorderSizePixel = value
-end
-
-function Component:get_ClipsDescendants()
-	return self.RootObject.ClipsDescendants
-end
-
-function Component:set_ClipsDescendants(value)
-	self.RootObject.ClipsDescendants = value
-end
-
-function Component:get_Draggable()
-	return self.RootObject.Draggable
-end
-
-function Component:set_Draggable(value)
-	self.RootObject.Draggable = value
-end
-
-function Component:get_NextSelectionDown()
-	return self.RootObject.NextSelectionDown
-end
-
-function Component:set_NextSelectionDown(value)
-	self.RootObject.NextSelectionDown = value
-end
-
-function Component:get_NextSelectionLeft()
-	return self.RootObject.NextSelectionLeft
-end
-
-function Component:set_NextSelectionLeft(value)
-	self.RootObject.NextSelectionLeft = value
-end
-
-function Component:get_NextSelectionRight()
-	return self.RootObject.NextSelectionRight
-end
-
-function Component:set_NextSelectionRight(value)
-	self.RootObject.NextSelectionRight = value
-end
-
-function Component:get_NextSelectionUp()
-	return self.RootObject.NextSelectionUp
-end
-
-function Component:set_NextSelectionUp(value)
-	self.RootObject.NextSelectionUp = value
-end
-
-function Component:get_Position()
-	return self.RootObject.Position
-end
-
-function Component:set_Position(value)
-	self.RootObject.Position = value
-end
-
-function Component:get_Rotation()
-	return self.RootObject.Rotation
-end
-
-function Component:set_Rotation(value)
-	self.RootObject.Rotation = value
-end
-
-function Component:get_Selectable()
-	return self.RootObject.Selectable
-end
-
-function Component:set_Selectable(value)
-	self.RootObject.Selectable = value
-end
-
-function Component:get_SelectionImageObject()
-	return self.RootObject.SelectionImageObject
-end
-
-function Component:set_SelectionImageObject(value)
-	self.RootObject.SelectionImageObject = value
-end
-
-function Component:get_Size()
-	return self.RootObject.Size
-end
-
-function Component:set_Size(value)
-	self.RootObject.Size = value
-end
-
-function Component:get_SizeConstraint()
-	return self.RootObject.SizeConstraint
-end
-
-function Component:set_SizeConstraint(value)
-	self.RootObject.SizeConstraint = value
-end
-
-function Component:get_Visible()
-	return self.RootObject.Visible
-end
-
-function Component:set_Visible(value)
-	self.RootObject.Visible = value
-end
-
-function Component:get_ZIndex()
-	return self.RootObject.ZIndex
-end
-
-function Component:set_ZIndex(value)
-	self.RootObject.ZIndex = value
+for _, property in ipairs(properties) do
+	Component["get_"..property] = function(self)
+		return self.RootObject[property]
+	end
+	
+	if not readOnlyProperties[property] then
+		Component["set_"..property] = function(self, value)
+			self.RootObject[property] = value
+		end
+	end
 end
 
 return Component
