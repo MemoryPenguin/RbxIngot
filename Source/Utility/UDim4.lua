1840aeb79e6d28365dbede5cd167ed6ddc3f6917
Narrev <NarrevRBX@gmail.com>
2016-11-22 18:02:06 -0600

    Update UDim4.lua

--- Source/Utility/UDim4.lua
+++ Source/Utility/UDim4.lua
@@ -1,58 +1,38 @@
--- @author MemoryPenguin
 -- @author Narrev
+-- @original MemoryPenguin
 -- UDim4 Object
 
-local newVector2 = Vector2.new
-local newUDim2 = UDim2.new
-local IsA = game.IsA
-local SizePos = {
-	Size = "AbsoluteSize";
-	Position = "AbsolutePosition";
-}
-
-local function Connect(Event, func)
-	Connect = Event.Connect
-	Connect(Event, func)
-end
-
-local function Disconnect(Event)
-	Disconnect = Event.Disconnect
-	Disconnect(Event)
-end
-
-local function ToUDim2(self, GuiObject)
-	if not GuiObject.Parent or not IsA(GuiObject.Parent, "GuiBase2d") then
-		return newVector2(0, 0)
+local function ToUDim2(self, guiObject)
+	if not guiObject.Parent or not guiObject.Parent:IsA("GuiBase2d") then
+		return UDim2.new()
 	end
 
-	local ParentSize = GuiObject.Parent.AbsoluteSize
-	local X, Y, XX, XY, YX, YY = ParentSize.X, ParentSize.Y, self.XX, self.XY, self.YX, self.YY
-	return newUDim2(
+	local parentSize = guiObject.Parent.AbsoluteSize
+	local X, Y, XX, XY, YX, YY = parentSize.X, parentSize.Y, self.XX, self.XY, self.YX, self.YY
+	return UDim2.new(
 		0, XX.Offset + XY.Offset + XX.Scale*X + XY.Scale*Y,
 		0, YY.Offset + YX.Offset + YY.Scale*Y + YX.Scale*X
 	)
 end
 
+-- Renders position and size
 local function Render(self)
-	--- Renders position and size
-
-	local Object = self.Object
-	Object.Position = ToUDim2(self.AbsolutePosition, Object)
-	Object.Size = ToUDim2(self.AbsoluteSize, Object)
+	local object = self._object
+	object.Position = ToUDim2(self._position, object)
+	object.Size = ToUDim2(self._size, object)
 end
 
+-- Attaches to object's parent and intercepts resize events there
 local function AttachToParent(self)
-	--- Attaches to object's parent and intercepts resize events there
-
-	local Parent = self.Object.Parent
+	local parent = self._object.Parent
 
 	if self._parentConnection then
-		Disconnect(self._parentConnection)
+		self._parentConnection:Disconnect()
 	end
 
-	if Parent then
-		self._parentConnection = Connect(Parent.Changed, function(Property)
-			if Property == "AbsoluteSize" or Property == "AbsolutePosition" then
+	if parent then
+		self._parentConnection = parent.Changed:Connect(function(property)
+			if property == "AbsoluteSize" or property == "AbsolutePosition" then
 				Render(self)
 			end
 		end)
@@ -62,56 +42,63 @@ local function AttachToParent(self)
 	return self
 end
 
-function newUDim4(X, Y, ...)
-	if ... then
-		return setmetatable({X = newUDim2(X, Y, ...); Y = newUDim2(select(3, ...))}, UDim4)
-	else
-		return setmetatable({X = X or newUDim2(); Y = Y or newUDim2()}, UDim4)
-	end
-end
-
 -- UDim4Render Class
 local UDim4Render = {Render = Render}
 
 function UDim4Render:__index(i)
-	return UDim4Render[i] or self[SizePos[i]]
+	if i == "Size" then
+		return self._size
+	elseif i == "Position" then
+		return self._position
+	else
+		return UDim4Render[i]
+	end
 end
 
 function UDim4Render:__newindex(i, v)
-	self[SizePos[i]] = v -- Only allows you to change Position and Size
-	Render(self)
+	if i == "Size" then
+		self._size = v
+		Render(self)
+	elseif i == "Position" then
+		self._position = v
+		Render(self)
+	end
 end
 
+-- Halts the enforcer
 function UDim4Render:Destroy()
-	--- Halts the enforcer
-	Disconnect(self._objectConnection)
+	self._objectConnection:Disconnect()
 	if self._parentConnection then
-		Disconnect(self._parentConnection)
+		self._parentConnection:Disconnect()
 	end
-
-	-- Release references
-	self.Object, self.AbsoluteSize, self.AbsolutePosition, self._objectConnection, self._parentConnection = nil
+	self._object, self._size, self._position, self._objectConnection, self._parentConnection = nil
 end
 
 -- UDim4 Class
-local UDim4 = {new = newUDim4}
+local UDim4 = {ToUDim2 = ToUDim2}
 UDim4.__index = UDim4
 
-function UDim4:ToVector2(GuiObject)
-	local Dim = ToUDim2(self, GuiObject)
-	return newVector2(Dim.X.Offset, Dim.Y.Offset)
+function UDim4.new(x, y, ...)
+	if ... then
+		return setmetatable({X = UDim2.new(x, y, ...); Y = UDim2.new(select(3, ...))}, UDim4)
+	else
+		return setmetatable({X = x or UDim2.new(); Y = y or UDim2.new()}, UDim4)
+	end
 end
 
-function UDim4:MakeEnforcer(GuiObject, Position, Size)
+local defaultSize = UDim4.new(UDim2.new(0, 0, 1, 0), UDim2.new(0, 0, 0, 0))
+local defaultPosition = UDim4.new(UDim2.new(1, 0, -1, 0), UDim2.new(0, 0, 1, 0))
+
+function UDim4.MakeEnforcer(guiObject, position, size)
 	self = {
-		Object = GuiObject;
-		AbsoluteSize = Size or newUDim4(newUDim2(0, 0, 1, 0), newUDim2(0, 0, 0, 0));
-		AbsolutePosition = Position or newUDim4(newUDim2(1, 0, -1, 0), newUDim2(0, 0, 1, 0));
-		_parentConnection = true;
+		_object = guiObject;
+		_size = size or defaultSize;
+		_position = position or defaultPosition;
+		_parentConnection = false;
 	}
 
-	self._objectConnection = Connect(GuiObject.Changed, function(Property)
-		if Property == "AbsoluteSize" or Property == "AbsolutePosition" then
+	self._objectConnection = guiObject.Changed:Connect(function(property)
+		if property == "AbsoluteSize" or property == "AbsolutePosition" then
 			Render(self)
 		elseif Property == "Parent" then
 			AttachToParent(self)
